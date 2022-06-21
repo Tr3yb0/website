8fed65f80b64d967feb528cad92551affe7c668a
Trey Botard <trey.botard@gmail.com>
2022-06-20 23:30:32 -0400

    Add tailwind font config

--- pages/main.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Main page</h1>
-</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  mode: "jit",
+  content: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  fontFamily: {
+    sans: ['Roboto', 'sans-serif'],
+    serif: ['Merriweather', 'serif']
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'h1': { fontSize: theme('fontSize.2xl') },
+        'h2': { fontSize: theme('fontSize.xl') },
+        'h3': { fontSize: theme('fontSize.lg') },
+      })
+    })
+  ]
+};
