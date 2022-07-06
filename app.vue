7f5054d6dae61fced82d5d433e098f64c221d859
Trey Botard <trey.botard@gmail.com>
2022-07-05 23:50:15 -0400

    Add header comp using nuxi
    Update script to follow conv from nuxi

--- app.vue
+++ app.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import '~/assets/css/tailwind.css';
 </script>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,6 @@ module.exports = {
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
     'app.vue',
   ],
   fontFamily: {
