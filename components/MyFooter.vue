8a22d2b87108b8b00854b367055f0f690e107cfa
Trey Botard <trey.botard@gmail.com>
2023-02-20 18:08:21 -0500

    add socials icon buttons

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -1,8 +1,11 @@
+<script lang="ts" setup></script>
+
 <template>
-  <footer>
-    <NuxtLink to="https://github.com/tr3yb0"> Github </NuxtLink>
-    <NuxtLink to="https://twitter.com/treybotard"> Twitter </NuxtLink>
-    <NuxtLink to="https://linkedin.com/in/treybotard"> LinkedIn </NuxtLink>
-    <NuxtLink to="https://www.instagram.com/tr3yb0/"> Insta </NuxtLink>
+  <footer class="flex flex-row">
+    <IconsGithubButton />
+    <IconsTwitterButton />
+    <IconsInstaButton />
+    <IconsYouTubeButton />
+    <IconsLinkedInButton />
   </footer>
 </template>
