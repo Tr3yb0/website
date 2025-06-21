f93a1d13fd6659da03a4feb4211e61c7a5090342
Trey Botard <trey.botard@gmail.com>
2025-06-21 15:16:21 -0400

    add sticky leftnav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-row justify-center items-start max-w-3xl mx-auto pt-32">
-    <LeftNav class="m-4 flex-none w-36" />
+    <LeftNav class="m-4 flex-none w-36 sticky top-8" />
     <slot />
   </div>
 </template>
