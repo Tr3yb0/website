b232a0c7a356636328e9306f60bfad72a803188b
Trey Botard <trey.botard@gmail.com>
2025-06-20 23:36:58 -0400

    add Speaking & Community section component
    Created new Speaking.vue component featuring:
    - Conference speaking engagements (Knowledge Graph Conference, FinovateSpring)
    - Podcast appearances (Fireside with Voxgig Episode 238)
    - Advisory board membership (DevNetwork Developer Evangelist/Advocate Board)

---
+++ components/Speaking.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="mt-12">
+    <h2 class="text-xl font-semibold mb-6">Speaking & Community</h2>
+    
+    <div class="space-y-6">
+      <div class="border-l-2 border-gray-600 pl-4">
+        <h3 class="font-medium text-lg">Conference Speaking</h3>
+        <ul class="mt-2 space-y-2 text-gray-300">
+          <li>• <b>The Knowledge Graph Conference</b> - Speaker</li>
+          <li>• <b>FinovateSpring</b> - Featured Speaker</li>
+        </ul>
+      </div>
+      
+      <div class="border-l-2 border-gray-600 pl-4">
+        <h3 class="font-medium text-lg">Podcast Appearances</h3>
+        <ul class="mt-2 space-y-2 text-gray-300">
+          <li>• <b>Fireside with Voxgig</b> - Episode 238: "Head of Developer Relations at Atomic"</li>
+        </ul>
+      </div>
+      
+      <div class="border-l-2 border-gray-600 pl-4">
+        <h3 class="font-medium text-lg">Advisory & Leadership</h3>
+        <ul class="mt-2 space-y-2 text-gray-300">
+          <li>• <b>DevNetwork</b> - Developer Evangelist/Advocate Advisory Board Member</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
