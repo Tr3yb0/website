ac7a8ef6a9c3ecdb0ed05aa275cddd8eddc9286e
Trey Botard <trey.botard@gmail.com>
2025-06-21 15:52:23 -0400

    update bio content

--- components/Bio.vue
+++ components/Bio.vue
@@ -1,26 +1,34 @@
 <template>
   <h1 class="font-serif">Trey Botard</h1>
   <p class="mt-8">
-    I'm the
+    I lead
     <b
-      >Director of Developer Relations at
+      >Developer Relations at
       <NuxtLink
-        class="hover:underline"
+        class="hover:underline hover:opacity-100 text-blue-400 transition-opacity"
         to="https://atomic.financial"
-        target="_blank">
+        target="_blank"
+      >
         Atomic Financial</NuxtLink
-      ></b
-    >, where I manage the DX of developers building products using our payroll
-    integrations and digital financial connectivity platform.
+      >
+    </b>
+    simplifying financial connectivity so tech teams can build and ship fast.
   </p>
   <p class="mt-6">
-    With over five years of experience in the tech industry, I'm dedicated to empowering developers to build remarkable innovations. I previously served as a Developer Advocate at 
-    <b>Fluree</b>, a semantic graph database company, and have developed software solutions at 
-    <b>Lincoln Financial</b>, <b>Volvo</b>, and <b>Bank of America</b> across areas including natural language understanding, automation, and data integrations.
+    With nearly a decade of experience in the tech industry, I'm dedicated to
+    empowering developers to craft delightful, connected experiences. Before
+    joining <b>Atomic</b> as a Developer Advocate, I served as a Developer
+    Advocate at <b>Fluree</b>, a semantic graph database company, and have
+    developed software solutions at <b>Lincoln Financial</b>, <b>Volvo</b>, and
+    <b>Bank of America</b> across areas including web development, natural
+    language understanding, automation, and data integrations.
   </p>
   <p class="mt-6">
-    Building tools and creating content of various types for developers has
-    become a passion of mine. I strive to understand and streamline the
-    end-to-end journey of developers to make their work lives better through accessible tools, community engagement, and developer advocacy.
+    Building tools that help technical teams succeed has become a passion of
+    mine; whether that tool is a well-architected docs site, a demo repo, a
+    thoughtful diagram, a clear presentation of an integration architecture. I
+    lead with empathy, aiming to understand and streamline the end-to-end
+    journey of developers to make their work lives more efficient, productive,
+    and focused on outcomes, not obstacles.
   </p>
 </template>
